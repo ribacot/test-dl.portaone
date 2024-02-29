02d413c99190d546247ad8bf1bcb86f1b6f6d2ba
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-03-01 00:18:27 +0200

    finding maxSubsequences

--- findMax.js
+++
@@ -1,3 +0,0 @@
-export default function getMaxOfArray(arr) {
-    return Math.max.apply(null,arr)
-}
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const fileInput = document.getElementById("fileInput");
 
-
-
 function findMediana(arr) {
 	const mid = Math.floor(arr.length / 2);
 	if (arr.length % 2) {
@@ -11,15 +9,47 @@ function findMediana(arr) {
 	}
 }
 
-function findIncreaseSubsequence(arr){
-	arr.reduce((acc,el)=> { 
-		
-		return},[])
+function findIncreaseSubsequence(arr, value) {
+	let resultArr = [];
+	let cacheArr = [];
+	let a = null;
+
+	for (let i = 1; i < arr.length; i += 1) {
+		switch (value) {
+			case "+":
+				a = arr[i] - arr[i - 1] <= 0;
+				break;
+			case "-":
+				a = arr[i] - arr[i - 1] >= 0;
+				break;
+			default:
+				console.log("no value");
+				return;
+		}
+
+		if (a) {
+			if (cacheArr.length) {
+				resultArr.push(cacheArr);
+			}
+			cacheArr = [];
+			continue;
+		}
+
+		if (!cacheArr.length) {
+			cacheArr.push(arr[i - 1]);
+		}
+		cacheArr.push(arr[i]);
+	}
+	resultArr.push(cacheArr);
+
+	const reasultArrSort = resultArr.sort((a, b) => a.length - b.length);
+
+	return reasultArrSort.filter(
+		(el) => el.length === reasultArrSort[reasultArrSort.length - 1].length
+	);
 }
 
-
-
-fileInput.addEventListener("change", function (e) {
+fileInput.addEventListener("change", function () {
 	const file = fileInput.files[0];
 	const textType = /text.*/;
 
@@ -40,10 +70,16 @@ fileInput.addEventListener("change", function (e) {
 				}) / sortArr.length;
 			const medianaValue = findMediana(sortArr);
 
+			const maxIncreaseыSubsequences = findIncreaseSubsequence(numberArr, "+");
+			const maxDecreasesSubsequences = findIncreaseSubsequence(numberArr, "-");
+
 			console.log("minValue", minValue);
 			console.log("maxValue", maxValue);
 			console.log("medianaValue", medianaValue);
 			console.log("averageValue", averageValue);
+
+			console.log("maxIncreaseыSubsequences", maxIncreaseыSubsequences);
+			console.log("maxDecreasesSubsequences", maxDecreasesSubsequences);
 		};
 	} else {
 		fileDisplayArea.innerText = "File not supported!";
