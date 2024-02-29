fe224a816b18b86891ee82ca67d0f00521f5fa78
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-02-29 19:56:39 +0200

    4 values

---
+++ findMax.js
@@ -0,0 +1,3 @@
+export default function getMaxOfArray(arr) {
+    return Math.max.apply(null,arr)
+}
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 const fileInput = document.getElementById("fileInput");
 
-let contextArr = [];
+
+
+function findMediana(arr) {
+	const mid = Math.floor(arr.length / 2);
+	if (arr.length % 2) {
+		return arr[mid];
+	} else {
+		return (arr[mid - 1] + arr[mid]) / 2;
+	}
+}
+
+function findIncreaseSubsequence(arr){
+	arr.reduce((acc,el)=> { 
+		
+		return},[])
+}
+
+
+
 fileInput.addEventListener("change", function (e) {
 	const file = fileInput.files[0];
 	const textType = /text.*/;
+
 	if (file.type.match(textType)) {
 		const reader = new FileReader();
+
 		reader.readAsText(file);
+
 		reader.onload = function () {
-			contextArr = reader.result.split("\n");
-            // console.log( contextText)
+			const numberArr = reader.result.split("\n").map((el) => Number(el));
+			const sortArr = [...numberArr].sort((a, b) => a - b);
+			const minValue = sortArr[0];
+			const maxValue = sortArr[sortArr.length - 1];
 
+			const averageValue =
+				sortArr.reduce((acc, el) => {
+					return acc + el;
+				}) / sortArr.length;
+			const medianaValue = findMediana(sortArr);
 
-			// contextArr = contextText.split();
-			// console.log("contextArrr", contextArr);
+			console.log("minValue", minValue);
+			console.log("maxValue", maxValue);
+			console.log("medianaValue", medianaValue);
+			console.log("averageValue", averageValue);
 		};
 	} else {
 		fileDisplayArea.innerText = "File not supported!";
