d93193199583db893713975865af43c34d35810a
Andrii_fsd <122934616+ribacot@users.noreply.github.com>
2024-03-08 01:48:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,116 @@
-const fileInput = document.getElementById("fileInput");
+// const fileInput = document.getElementById("fileInput");
 
-function findAverageValue(arr) {
-	return (
-		arr.reduce((acc, el) => {
-			return acc + el;
-		}) / sortArr.length
-	);
-}
+// function findAverageValue(arr) {
+// 	return (
+// 		arr.reduce((acc, el) => {
+// 			return acc + el;
+// 		}) / sortArr.length
+// 	);
+// }
 
-function findMediana(arr) {
-	const mid = Math.floor(arr.length / 2);
-	if (arr.length % 2) {
-		return arr[mid];
-	} else {
-		return (arr[mid - 1] + arr[mid]) / 2;
-	}
-}
+// function findMediana(arr) {
+// 	const mid = Math.floor(arr.length / 2);
+// 	if (arr.length % 2) {
+// 		return arr[mid];
+// 	} else {
+// 		return (arr[mid - 1] + arr[mid]) / 2;
+// 	}
+// }
 
-function findIncreaseSubsequence(arr, value) {
-	let resultArr = [];
-	let cacheArr = [];
-	let condition = null;
+// function findIncreaseSubsequence(arr, value) {
+// 	let resultArr = [];
+// 	let cacheArr = [];
+// 	let condition = null;
 
-	for (let i = 1; i < arr.length; i += 1) {
+// 	for (let i = 1; i < arr.length; i += 1) {
 
-		switch (value) {
-			case "+":
-				condition = arr[i] - arr[i - 1] <= 0;
-				break;
-			case "-":
-				condition = arr[i] - arr[i - 1] >= 0;
-				break;
-			default:
-				console.log("no value");
-				return;
-		}
+// 		switch (value) {
+// 			case "+":
+// 				condition = arr[i] - arr[i - 1] <= 0;
+// 				break;
+// 			case "-":
+// 				condition = arr[i] - arr[i - 1] >= 0;
+// 				break;
+// 			default:
+// 				console.log("no value");
+// 				return;
+// 		}
 
-		if (condition) {
-			if (cacheArr.length) {
-				resultArr.push(cacheArr);
-			}
-			cacheArr = [];
+// 		if (condition) {
+// 			if (cacheArr.length) {
+// 				resultArr.push(cacheArr);
+// 			}
+// 			cacheArr = [];
+// 			continue;
+// 		}
+
+// 		if (!cacheArr.length) {
+// 			cacheArr.push(arr[i - 1]);
+// 		}
+// 		cacheArr.push(arr[i]);
+// 	}
+// 	resultArr.push(cacheArr);
+
+// 	const reasultArrSort = resultArr.sort((a, b) => a.length - b.length);
+
+// 	return reasultArrSort.filter(
+// 		(el) => el.length === reasultArrSort[reasultArrSort.length - 1].length
+// 	);
+// }
+
+// fileInput.addEventListener("change", function () {
+// 	const file = fileInput.files[0];
+// 	const textType = /text.*/;
+
+// 	if (file.type.match(textType)) {
+// 		const reader = new FileReader();
+
+// 		reader.readAsText(file);
+
+// 		reader.onload = function () {
+// 			const numberArr = reader.result.split("\n").map((el) => Number(el));
+// 			const sortArr = [...numberArr].sort((a, b) => a - b);
+// 			const minValue = sortArr[0];
+// 			const maxValue = sortArr[sortArr.length - 1];
+// 			const averageValue = findAverageValue(sortArr);
+// 			const medianaValue = findMediana(sortArr);
+
+// 			const maxIncreaseыSubsequences = findIncreaseSubsequence(numberArr, "+");
+// 			const maxDecreasesSubsequences = findIncreaseSubsequence(numberArr, "-");
+
+// 			console.log("minValue", minValue);
+// 			console.log("maxValue", maxValue);
+// 			console.log("medianaValue", medianaValue);
+// 			console.log("averageValue", averageValue);
+
+// 			console.log("maxIncreaseыSubsequences", maxIncreaseыSubsequences);
+// 			console.log("maxDecreasesSubsequences", maxDecreasesSubsequences);
+// 		};
+// 	} else {
+// 		fileDisplayArea.innerText = "File not supported!";
+// 	}
+// });
+const arrValues = [1, 2, 5, 5, 5, 1, 1, 6, 6, 8, 7, 9, 10, 55, 55];
+function fn(arr) {
+	let cachArr = [];
+	const resulArr = [];
+	for (let i = 0; i < arr.length; i += 1) {
+		cachArr = [];
+
+		if (resulArr.some(({ key }) => key === arr[i])) {
 			continue;
 		}
 
-		if (!cacheArr.length) {
-			cacheArr.push(arr[i - 1]);
+		for (let j = 0; j < arr.length; j += 1) {
+			if (arr[i] === arr[j]) {
+				cachArr.push(arr[i]);
+			}
 		}
-		cacheArr.push(arr[i]);
+		if (cachArr.length) {
+			resulArr.push({ key: arr[i], value: cachArr.length });
+		}
 	}
-	resultArr.push(cacheArr);
-
-	const reasultArrSort = resultArr.sort((a, b) => a.length - b.length);
-
-	return reasultArrSort.filter(
-		(el) => el.length === reasultArrSort[reasultArrSort.length - 1].length
-	);
+	console.log(resulArr);
 }
 
-fileInput.addEventListener("change", function () {
-	const file = fileInput.files[0];
-	const textType = /text.*/;
-
-	if (file.type.match(textType)) {
-		const reader = new FileReader();
-
-		reader.readAsText(file);
-
-		reader.onload = function () {
-			const numberArr = reader.result.split("\n").map((el) => Number(el));
-			const sortArr = [...numberArr].sort((a, b) => a - b);
-			const minValue = sortArr[0];
-			const maxValue = sortArr[sortArr.length - 1];
-			const averageValue = findAverageValue(sortArr);
-			const medianaValue = findMediana(sortArr);
-
-			const maxIncreaseыSubsequences = findIncreaseSubsequence(numberArr, "+");
-			const maxDecreasesSubsequences = findIncreaseSubsequence(numberArr, "-");
-
-			console.log("minValue", minValue);
-			console.log("maxValue", maxValue);
-			console.log("medianaValue", medianaValue);
-			console.log("averageValue", averageValue);
-
-			console.log("maxIncreaseыSubsequences", maxIncreaseыSubsequences);
-			console.log("maxDecreasesSubsequences", maxDecreasesSubsequences);
-		};
-	} else {
-		fileDisplayArea.innerText = "File not supported!";
-	}
-});
+fn(arrValues);
